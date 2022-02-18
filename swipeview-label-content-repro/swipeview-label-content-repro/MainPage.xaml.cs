eaec08e1c3d184b71f993ec91020e9b0895a2532
Sam Jordan <sjordan@gsspos.co.uk>
2022-02-18 08:55:27 +0000

    Add project files.

---
+++ swipeview-label-content-repro/swipeview-label-content-repro/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace swipeview_label_content_repro
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            swipe1.Open(OpenSwipeItem.LeftItems);
+            swipe2.Open(OpenSwipeItem.LeftItems);
+        }
+    }
+}
